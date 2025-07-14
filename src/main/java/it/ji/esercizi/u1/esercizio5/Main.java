abc44ce13f7cd1e3931f115b155e1d77244f7aaf
sommovir <luca.coraci@gmail.com>
2025-07-14 19:14:09 +0200

    testo esercizi

---
+++ src/main/java/it/ji/esercizi/u1/esercizio5/Main.java
@@ -0,0 +1,12 @@
+package it.ji.esercizi.u1.esercizio5;
+
+/*
+Definisci una classe Libro contenente i seguenti attributi: nome del libro, prezzo, numero di scaffale,
+ numero di pagine, casa editrice.
+   Definisci inoltre i metodi inizializza(), stampa(), applicaSconto(), che hanno i seguenti compiti:
+   – inizializzare i campi dati dell’oggetto classe;
+   – stampare tutti i dati dell’oggetto;
+   – diminuire del x% il prezzo del libro in oggetto.
+ */
+public class Main {
+}
---
+++ src/main/java/it/ji/esercizi/u1/esercizio6/Main.java
@@ -0,0 +1,12 @@
+package it.ji.esercizi.u1.esercizio6;
+
+/*
+Data la classe Libro per rappresentare oggetti Libro con il nome dell’autore, il titolo e il numero di
+ pagine e con i relativi metodi d’istanza, scrivi i seguenti metodi:– un metodo che, ricevendo come parametro un array di oggetti Libro, calcola e restituisce una
+ struttura collegata con gli stessi oggetti Libro nello stesso ordine;– un metodo che, ricevendo come parametro una struttura collegata di oggetti Libro, e il nome di
+ un autore, verifica se nell’elenco esista almeno un libro dell’autore dato;– un metodo che, ricevendo come parametro un codice ISBN, elimina il libro corrispondente dal
+l’elenco;
+   – un metodo che ordina i libri rispetto al numero crescente di pagine
+ */
+public class Main {
+}
