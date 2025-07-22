9d3b1349799249e6004443a5005a8ba1904fff50
sommovir <luca.coraci@gmail.com>
2025-07-22 11:27:43 +0200

    aggiornato testo esercizio 7

--- src/main/java/it/ji/esercizi/u1/esercizio7/Main.java
+++ src/main/java/it/ji/esercizi/u1/esercizio7/Main.java
@@ -1,12 +1,15 @@
 package it.ji.esercizi.u1.esercizio7;
 
 /*
- Definisci una classe Libro contenente i seguenti attributi: nome del libro, prezzo, numero di scaffale,
- numero di pagine, casa editrice.
-   Definisci inoltre i metodi inizializza(), stampa(), applicaSconto(), che hanno i seguenti compiti:
-   – inizializzare i campi dati dell’oggetto classe;
-   – stampare tutti i dati dell’oggetto;
-   – diminuire del x% il prezzo del libro in oggetto
+ Definisci una classe Prenotazione (di un campo da tennis) contenente il nome del cliente e l’ora
+ della sua prenotazione; a tal fine implementa una classe Campo in cui ci siano i seguenti metodi:
+   – public boolean addPren(int inizio, int fine, String unNomeCliente), per prenotare il campo, che
+ controlla se i dati inseriti sono giusti e se il campo è disponibile e, quindi, salva la prenotazione e
+ restituisce true se il campo è stato prenotato;
+   – public boolean removePren (int inizio, int fine, String unNomeCliente), per cancellare una preno
+tazione, che controlla se il campo è stato prenotato dal cliente che vuole cancellare la prenotazione
+ e, quindi, la cancella e restituisce true se la prenotazione è stata cancellata;
+   – public double utilizzo(), per trovare la percentuale di utilizzo del campo.
 */
 public class Main {
     public static void main(String[] args) {
